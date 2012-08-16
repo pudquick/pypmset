96aa869ef47801068e970f4dece3f1c16e08c6f2
Michael Lynn <pudquick@sogetthis.com>
2012-08-16 11:00:01 -0700

    Updated to use the provided CoreFoundation wrapper

--- pypmset.py
+++ pypmset.py
@@ -1,32 +1,35 @@
 from ctypes import c_uint32, cdll, c_int, c_void_p, POINTER, byref
 from ctypes.util import find_library
-
-import CFStringUtil as cfsu
-CFSTR = cfsu.create_cfstringref
-CFRELEASE = cfsu.release_cfstringref
+from CoreFoundation import CFStringCreateWithCString, CFRelease
+from objc import pyobjc_id
 
 libIOKit = cdll.LoadLibrary(find_library("/System/Library/Frameworks/IOKit.framework/IOKit"))
-
 libIOKit.IOPMAssertionCreateWithName.argtypes = [ c_void_p, c_uint32, c_void_p, POINTER(c_uint32) ]
 libIOKit.IOPMAssertionCreateWithName.restype = c_int
 libIOKit.IOPMAssertionRelease.argtypes = [ c_uint32 ]
 libIOKit.IOPMAssertionRelease.restype = c_int
 
+def CFSTR(py_string):
+    return CFStringCreateWithCString(None, py_string, 0L)
+
+def raw_ptr(pyobjc_string):
+    return pyobjc_id(pyobjc_string.nsstring())
+
 kIOPMAssertionTypeNoDisplaySleep = CFSTR("NoDisplaySleepAssertion")
 kIOPMAssertionTypeNoIdleSleep = CFSTR("NoIdleSleepAssertion")
 kIOPMAssertionLevelOn = c_uint32(255)
 kIOPMAssertionLevelOff = c_uint32(0)
-
 assertID = c_uint32(0)
 reason = CFSTR("python would like the computer to not idle sleep")
 
 # Stop idle sleep
-errcode = libIOKit.IOPMAssertionCreateWithName(kIOPMAssertionTypeNoIdleSleep, kIOPMAssertionLevelOn, reason, byref(assertID))
+errcode = libIOKit.IOPMAssertionCreateWithName(raw_ptr(kIOPMAssertionTypeNoIdleSleep),
+    kIOPMAssertionLevelOn, raw_ptr(reason), byref(assertID))
 
 # Let it go again
 errcode = libIOKit.IOPMAssertionRelease(assertID)
 
 # Clean up
-CFRELEASE(kIOPMAssertionTypeNoDisplaySleep)
-CFRELEASE(kIOPMAssertionTypeNoIdleSleep)
-CFRELEASE(reason)
+CFRelease(kIOPMAssertionTypeNoDisplaySleep)
+CFRelease(kIOPMAssertionTypeNoIdleSleep)
+CFRelease(reason)
